eaa9ef6ce068c10d0495069d9822265f119c87e1
m1ka39 <michaelabisajr@gmail.com>
2026-05-14 20:03:36 -0700

    Update summarize function to use new API endpoint

--- processing.js
+++ processing.js
@@ -7,7 +7,7 @@ async function summarize() {
             return;
         }
 
-        const response = await fetch("http://127.0.0.1:5000/summarize", {
+        const response = await fetch("https://mikes-first-backend.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
